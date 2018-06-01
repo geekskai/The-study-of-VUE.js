f3dedaacdc830aa0e3416d08dd073e7affa057f3
Boss gan <gk1424301572@163.com>
2018-06-01 22:37:42 +0800

    add new where to go---

---
+++ Study-vue.js/day02/code/push.vue
@@ -0,0 +1,32 @@
+<!--  -->
+<template>
+  <div >
+<ul>
+  <li v-for="item in list">{{item.name}}-------->{{item.price}}</li>
+</ul>
+<button @click="add"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      list:[{name:'张三',price:'234'},{name:'李四',price:'342'}]
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {add(){
+    console.log(this.list)
+  }}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
--- Study-vue.js/day03/code/demo.vue
+++ Study-vue.js/day03/code/demo.vue
@@ -2,7 +2,6 @@
 <template>
   <div></div>
 </template>
-
 <script>
 export default {
   data () {
