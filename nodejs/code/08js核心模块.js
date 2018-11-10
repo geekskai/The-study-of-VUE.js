e064205ccf11d207968f5a4b51bce8b7e98a12d8
Boss_Gan <gk1424301572@163.com>
2018-11-11 00:04:57 +0800

    nodejs相应头内容 Content-Type, text/plain charset:utf-8

---
+++ nodejs/code/08js核心模块.js
@@ -0,0 +1,5 @@
+var os = require('os');
+var path = require('path')
+console.log('你的CPU是:',os.cpus());
+console.log('你的路径是:',path.extname('E:\Data\codeSpace\The-study-of-VUE.js'));
+console.log('你的内存是:',os.totalmem());
